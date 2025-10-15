db3ae9dc8c45c854ed4b7713074103c8d605713f
omea92 <thdjun92@naver.com>
2025-10-16 00:48:20 +0900

    junit basic

---
+++ src/test/java/me/wonjun/springbootdeveloper/TestControllerTest.java
@@ -0,0 +1,34 @@
+package me.wonjun.springbootdeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@AutoConfigureMockMvc //배포없이 MVC환경 제공해주는 컨트롤러 테스트지원
+class TestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        memberRepository.deleteAll();
+    }
+}
